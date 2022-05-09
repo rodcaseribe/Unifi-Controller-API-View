d9895d3e3f8c10f029640941a61b8e086dbb0b43
rodcaseribeiro <rodolfo2804@hotmail.com>
2022-05-09 16:36:55 -0300

    Update roteador.php

--- Router/roteador.php
+++ Router/roteador.php
@@ -1,6 +1,9 @@
 <?php
-    if(isset($_COOKIE['CookieUnifi']) && $_SERVER['REQUEST_URI'] == '/unifiNewPortal/index.php' || $_SERVER['PHP_SELF'] == '/unifiNewPortal/index.php'){
+    if(isset($_COOKIE['CookieUnifi']) && $_SERVER['REQUEST_URI'] == '/unifiNewPortal/index.php' ){
         header("Location: painel.php");
+    }
+    else if($_SERVER['PHP_SELF'] == '/unifiNewPortal/index.php' && isset($_COOKIE['CookieUnifi'])){
+	header("Location: painel.php");
     }
     else if(!isset($_COOKIE['CookieUnifi']) && $_SERVER['REQUEST_URI'] == '/unifiNewPortal/painel.php'){
         echo "Usuário não logado clique <a href=\"index.php\">aqui<a/> para voltar";
